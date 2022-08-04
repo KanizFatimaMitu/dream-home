14e2887a067b3deb64a4f0343bd791e9ca11993d
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-08-05 02:58:59 +0600

    done with header

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = ({ children }) => {
       <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col">
 
-        <div class="w-full navbar bg-base-300">
-          <div class="flex-1 px-2 mx-2 text-2xl">Dream House</div>
+        <div class="w-full navbar px-20">
+          <div class="flex-1 px-2 mx-2 text-2xl text-primary font-bold">Dream House</div>
           <div class="flex-none lg:hidden">
             <label for="my-drawer-3" class="btn btn-square btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -17,14 +17,14 @@ const Navbar = ({ children }) => {
 
 
           <div class="flex-none hidden lg:block">
-            <ul class="menu menu-horizontal">
+            <ul class="menu menu-horizontal gap-x-2">
 
               <li><NavLink to="/" className="rounded-lg">Home</NavLink></li>
               <li><NavLink to="/packages" className="rounded-lg">Packages</NavLink></li>
               <li><NavLink to="/designs" className="rounded-lg">Designs</NavLink></li>
               <li><NavLink to="/about" className="rounded-lg">About</NavLink></li>
               <li><NavLink to="/bookings" className="rounded-lg">Bookings</NavLink></li>
-              <li><NavLink to="/signin" className="rounded-lg">SignIn</NavLink></li>
+              <li><NavLink to="/signin" className="rounded-lg border-2 border-primary">SignIn</NavLink></li>
             </ul>
           </div>
         </div>
@@ -34,14 +34,14 @@ const Navbar = ({ children }) => {
       </div>
       <div class="drawer-side">
         <label for="my-drawer-3" class="drawer-overlay"></label>
-        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 rounded-box">
+        <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 rounded-box gap-y-2">
 
           <li><NavLink to="/" className="rounded-lg">Home</NavLink></li>
           <li><NavLink to="/packages" className="rounded-lg">Packages</NavLink></li>
           <li><NavLink to="/designs" className="rounded-lg">Designs</NavLink></li>
           <li><NavLink to="/about" className="rounded-lg">About</NavLink></li>
           <li><NavLink to="/bookings" className="rounded-lg">Bookings</NavLink></li>
-          <li><NavLink to="/signin" className="rounded-lg">SignIn</NavLink></li>
+          <li><NavLink to="/signin" className="rounded-lg border-2 border-primary">SignIn</NavLink></li>
 
         </ul>
 
