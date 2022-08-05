5bcfc17ec94e4f93f95b148c0d04f26aab858805
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-08-06 00:00:08 +0600

    nav change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,9 @@ const Navbar = ({ children }) => {
       <div class="drawer-content flex flex-col">
 
         <div class="w-full navbar px-20">
-          <div class="flex-1 px-2 mx-2 text-2xl text-primary font-bold">Dream House</div>
+          <div class="flex-1 px-2 mx-2 text-2xl text-primary font-bold">
+            <NavLink to="/">Dream House</NavLink>
+          </div>
           <div class="flex-none lg:hidden">
             <label for="my-drawer-3" class="btn btn-square btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
