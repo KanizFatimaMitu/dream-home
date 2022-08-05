c67293174f9f1be8f46771e34e404ca40fc3ae2d
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-08-06 02:21:56 +0600

    online/offline

--- src/Pages/Bookings.js
+++ src/Pages/Bookings.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Bookings = () => {
     return (
         <div>
-            <h1>Your bookings</h1>
+            <h1>"I am a MERN stack web developer. I love to use my creativity and make something new. That's why I love to play with code. Over the last year, I gather knowledge in various parts of web development by myself. My ultimate goal is to become an awesome full-stack web developer who can make beautiful UIs and also can handle the back-end smoothly."</h1>
         </div>
     );
 };
