75e602ddc7a588b262042127a7ace5e1308fdafd
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-08-07 18:57:19 +0600

    animation done

--- src/Pages/Home/LandingPage.js
+++ src/Pages/Home/LandingPage.js
@@ -17,12 +17,38 @@ const LandingPage = () => {
     <div class="hero h-[90vh] bg-base-200 image">
 
       <div className='mr-72'>
-        <div className='w-3/5' data-aos="fade-right">
-          <h1 class="text-5xl text-primary font-bold">Your Dream</h1>
-          <h3 class="text-xl text-primary font-bold pt-2">Our responsibility</h3>
-          <p class="py-6 text-lg font-medium text-accent">"You can choose however you want.We'll provide you that at best budget in town."
+        <div className='w-3/5' >
+          <h1
+            class="text-5xl text-primary font-bold"
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            data-aos-delay="200"
+          >
+            Your Dream
+          </h1>
+          <h3
+            class="text-xl text-primary font-bold pt-2"
+            data-aos="fade-right"
+            data-aos-duration="900"
+            data-aos-delay="400"
+          >
+            Our responsibility
+          </h3>
+          <p
+            class="py-6 text-lg font-medium text-accent"
+            data-aos="fade-right"
+            data-aos-duration="800"
+            data-aos-delay="600"
+          >
+            "You can choose however you want.We'll provide you that at best budget in town."
           </p>
-          <button class="btn btn-primary text-bold">Login</button>
+          <button
+            class="btn btn-primary text-bold"
+            data-aos="zoom-in"
+            data-aos-delay="1300"
+          >
+            Please signin
+          </button>
         </div>
       </div>
     </div>
