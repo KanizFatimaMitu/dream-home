3feff85397cf72c846c256af757df5914b00fde4
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-08-07 23:45:39 +0600

    google sign in done

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -4,27 +4,30 @@ import './SignIn.css'
 const SignIn = () => {
     return (
 
-        <div className='sign-image h-[90vh] pt-16 '>
+        <div className='sign-image h-[90vh] pt-4 '>
             <div class="card flex-shrink-0 w-full max-w-sm ml-60 shadow-2xl bg-base-100 ">
                 <div class="card-body">
+                    <h3 className='text-center font-bold text-primary'>SignIn</h3>
                     <div class="form-control">
                         <label class="label">
-                            <span class="label-text">Email</span>
+                            <span class="label-text text-primary">Email</span>
                         </label>
                         <input type="text" placeholder="email" class="input input-bordered" />
                     </div>
                     <div class="form-control">
                         <label class="label">
-                            <span class="label-text">Password</span>
+                            <span class="label-text text-primary">Password</span>
                         </label>
                         <input type="text" placeholder="password" class="input input-bordered" />
                         <label class="label">
-                            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                            <a href="#" class="label-text-alt link link-hover text-primary">Forgot password?</a>
                         </label>
                     </div>
                     <div class="form-control mt-6">
                         <button class="btn btn-primary">signin</button>
                     </div>
+                    <div class="divider">OR</div>
+                    <button className='btn btn-glass hover:btn-primary'>continue with google</button>
                 </div>
             </div>
         </div>
