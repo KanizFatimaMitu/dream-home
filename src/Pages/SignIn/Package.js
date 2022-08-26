aadc6d721c61ec40635d645e763c218fefb38414
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-08-27 02:34:35 +0600

    modals done

--- src/Pages/SignIn/Package.js
+++ src/Pages/SignIn/Package.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const Package = ({ perPackage }) => {
     const { name, price } = perPackage;
@@ -13,14 +12,38 @@ const Package = ({ perPackage }) => {
                     <h2 class="card-title">{name}</h2>
                     <p>{price}</p>
                     <div class="card-actions">
-                        <button class="btn btn-primary">
-                            <NavLink>See Details</NavLink>
-                        </button>
-                        <button class="btn btn-primary">Book Now</button>
+                        {/* <!-- The button to open modal --> */}
+                        <label for="my-modal-6" class="btn modal-button btn-primary">See Details</label>
+
+                        {/* <!-- Put this part before </body> tag --> */}
+                        <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                        <div class="modal modal-bottom sm:modal-middle">
+                            <div class="modal-box">
+                                <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                                <div class="modal-action">
+                                    <label for="my-modal-6" class="btn">close</label>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- The button to open Book Now modal --> */}
+                        <label for="my-modal-6" class="btn btn-primary modal-button">Book Now</label>
+
+                        {/* <!-- Put this part before </body> tag --> */}
+                    <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+                    <div class="modal modal-bottom sm:modal-middle">
+                        <div class="modal-box">
+                            <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+                            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                            <div class="modal-action">
+                                <label for="my-modal-6" class="btn">Close</label>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        </div >
     );
 };
 
