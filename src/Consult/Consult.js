8ec1523c1ba0e06912089c2d311512d9f4369fcf
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-09-30 21:05:02 +0600

    day picker style was missed

--- src/Consult/Consult.js
+++ src/Consult/Consult.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { format } from 'date-fns';
 import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 import Footer from '../Components/Footer';
 
 
@@ -10,7 +11,7 @@ const Consult = () => {
     let footer = <p>Please pick a day.</p>;
     if (date) {
         footer = <p
-            class="btn btn-primary text-bold text-neutral"
+            class="btn btn-primary text-bold text-neutral m-4"
             data-aos="zoom-in"
             data-aos-delay="1300">
             You picked {format(date, 'PP')}.
@@ -24,13 +25,14 @@ const Consult = () => {
             <div
                 data-aos="flip-up"
                 class="hero min-h-screen bg-base-200">
-                <div class="hero-content flex-col lg:flex-row">
+                <div class="hero-content flex-col lg:flex-row m-8">
                     <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
                     <div
                         data-aos="fade-up"
                         data-aos-duration="1000"
                         data-aos-delay="400">
                         <DayPicker
+                        
                             mode="single"
                             selected={date}
                             onSelect={setDate}
