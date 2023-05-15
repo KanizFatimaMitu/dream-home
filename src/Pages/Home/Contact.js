fe28490a077a92bd7fcc4ed7f992a936a5b69ae1
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-05-15 22:20:13 +0600

    responsive done

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className='bg-primary  '>
-             <h1 className='text-center mt-32 text-4xl font-bold flex justify-center items-center p-16 text-white'>READY TO DESIGN YOUR SPACE? CALL NOW!<span className='ml-20 bg-white text-primary p-4'> +8801634243558</span></h1>
+             <h1 className='text-center mt-8 mb-8 lg:mt-32 lg:text-4xl text-sm font-bold flex justify-center items-center p-16 text-white'>READY TO DESIGN YOUR SPACE? CALL NOW!<span className='ml-20 bg-white text-primary p-4'> +8801634243558</span></h1>
         </div>
     );
 };
